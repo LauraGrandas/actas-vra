88cff8de35987f721b3e5f097e5ba42d51404e4c
Laura Grandas <lm.grandas@gmail.com>
2022-10-06 00:23:09 -0500

    includes the dataframe in streamlit

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import pandas as pd
-df = pd.read_excel('dummy-actas.xlsx')
+df = pd.read_csv("dummy-actas.csv", header = 0)
+
+palabra = "Informe"
+df_extract = df[df['tag'].str.contains("Informe")]
+objeto = df_extract['extracto']
--- myapp.py
+++ myapp.py
@@ -47,3 +47,11 @@ with st.sidebar:
         "Seleccione una etiqueta",
         categorias.all_tags
     )
+
+#ya fuera del sidebar
+df = pd.read_csv("dummy-actas.csv", header = 0)
+
+palabra = "Informe"
+df_extract = df[df['tag'].str.contains("Informe")]
+objeto = df_extract['extracto']#.to_list()
+st.write(objeto)
