5f3358ada4a1309f7637b27ad41849d932e545e9
Laura Grandas <lm.grandas@gmail.com>
2022-10-05 22:49:27 -0500

    creates categories for the tags

---
+++ categorias.py
@@ -0,0 +1,107 @@
+all_tags = {
+    "PDI",
+    "Estructura Interna",
+    "Ajustes Administrativos",
+    "semilleros",
+    "Formación en investigación",
+    "Promoción investigación",
+    "Creación artistica",
+    "Egresados",
+    "Posicionamiento",
+    "Creación Programas",
+    "Evaluación curricular",
+    "Educación General",
+    "Deserción",
+    "Politicas Academicas",
+    "Reglamentos",
+    "Estatuto profesoral",
+    "Acompañamiento AC Estudiantes",
+    "Perfil de estudiantes",
+    "Oferta de cursos",
+    "Educación continua",
+    "Oferta virtual",
+    "Oferta otras sedes",
+    "Admisión",
+    "Cambios administrativos",
+    "Fenicia, ¿regional?",
+    "Inversiones ",
+    "Infraestructura",
+    "Administrativos",
+    "Representación",
+    "Deportes",
+    "cultura",
+    "iniciativas estudiantiles",
+    "Bienestar"
+}
+
+all_vicerrectorias = {
+    "Rectoría",
+    "Vicerrectoría de investigación y creación",
+    "Vicerrectoría de Desarrollo y egresados",
+    "Vicerrectoría Academica",
+    "Vicerrectoría Financiera",
+    "DECA"
+}
+
+within_vicerrectorias = {
+    "Rectoría" : [
+        "PDI",
+        "Estructura Interna",
+        "Ajustes Administrativos"
+        ],
+    "Vicerrectoría de investigación y creación" : [
+        "semilleros",
+        "Formación en investigación",
+        "Promoción investigación",
+        "Creación artistica"
+        ],
+    "Vicerrectoría de Desarrollo y egresados" : [
+        "Egresados",
+        "Posicionamiento"
+        ],
+    "Vicerrectoría Academica": [
+        "Creación Programas",
+        "Evaluación curricular",
+        "Educación General",
+        "Deserción",
+        "Politicas Academicas",
+        "Reglamentos",
+        "Estatuto profesoral",
+        "Acompañamiento AC Estudiantes",
+        "Perfil de estudiantes",
+        "Oferta de cursos",
+        "Educación continua",
+        "Oferta virtual",
+        "Oferta otras sedes",
+        "Admisión"
+        ],
+    "Vicerrectoría Financiera": [
+        "Cambios administrativos",
+        "Fenicia, ¿regional?",
+        "Inversiones ",
+        "Infraestructura",
+        "Administrativos",
+        ],
+    "DECA" : [
+        "Representación",
+        "Deportes",
+        "cultura",
+        "iniciativas estudiantiles",
+        "Bienestar"
+    ]
+}
+
+all_actores = {
+    "Profesores",
+    "Estudiantes",
+    "Administrativos",
+    "Egresados",
+    "Otros empleados"
+}
+
+all_niveles = {
+    "Pregrado",
+    "Posgrado",
+    "Educación continua"
+}
+
---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+# create a header
+
+st.write(
+    """
+
+    ## Clasificación de actas del Consejo Académico de la Universidad de los Andes
+
+    """
+    )
